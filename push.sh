8dc948f2dee62c46516658afd2aa3e1ad880d611
Jone Vassbø <jone.vassbo@gmail.com>
2022-08-29 19:30:45 +0200

    Moved to v2

--- push.sh
+++ push.sh
@@ -1,5 +1,5 @@
 echo "Building app"
-docker build -f ./Api/Dockerfile -t jonev/home-hosting:masters-v1 .
-docker push jonev/home-hosting:masters-v1
+docker build -f ./Api/Dockerfile -t jonev/home-hosting:masters-v2 .
+docker push jonev/home-hosting:masters-v2
 ssh -t jone@prod1 '/home/jone/repos/home-hosting/masters/deploy.sh'
 echo "Successfully deployed the app"
