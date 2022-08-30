a4d577af16772135523394787c02ab46b3caef73
Jone Vassbø <jone.vassbo@gmail.com>
2022-08-30 21:49:15 +0200

    Adjusted nr of sheeps

--- Api/Services/DataSharingService.cs
+++ Api/Services/DataSharingService.cs
@@ -8,7 +8,7 @@ public class DataSharingService
     public Coordinate MousePosition { get; set; } = new Coordinate(0, 0);
     public bool Start { get; set; } = false;
     public bool Reset { get; set; } = false;
-    public int NrOfSheeps { get; set; } = 20;
+    public int NrOfSheeps { get; set; } = 100;
     public string Name { get; set; } = "Unknown";
     public ConcurrentBag<Score> ScoreBoard = new ConcurrentBag<Score>();
 
