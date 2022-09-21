c5ad39bb16baab78ed941ac75bf460c75d1fdcde
Jone Vassbø <jone.vassbo@gmail.com>
2022-09-21 22:15:54 +0200

    Create max rate of change function

---
+++ Api.Tests/MaxRateOfChangeTest.cs
@@ -0,0 +1,56 @@
+using FluentAssertions;
+using SheepHerding.Api.Helpers;
+
+namespace SheepHerding.Api.Tests;
+
+public class MaxRateOfChangeTest
+{
+    [Fact]
+    public void MaxRateOfChangeIncreaseTest()
+    {
+        var mrc = new MaxRateOfChange();
+        var input = 100.0;
+        var max = 10.0;
+
+        for (int i = 1; i <= 10; i++)
+        {
+            var result = mrc.Limit(input, max);
+            result.Should().BeApproximately(i * max, 0.01f);
+        }
+    }
+    
+    [Fact]
+    public void MaxRateOfChangeDecreaseTest()
+    {
+        var mrc = new MaxRateOfChange(100.0);
+        var input = 0.0;
+        var max = 10.0;
+
+        for (int i = 10; i <= 1; i--)
+        {
+            var result = mrc.Limit(input, max);
+            result.Should().BeApproximately(i * max, 0.01f);
+        }
+    }
+    
+    [Fact]
+    public void MaxRateOfChangeBothTest()
+    {
+        var mrc = new MaxRateOfChange();
+        var input = 100.0;
+        var max = 10.0;
+
+        for (int i = 1; i <= 10; i++)
+        {
+            var result = mrc.Limit(input, max);
+            result.Should().BeApproximately(i * max, 0.01f);
+        }
+
+        input = 0.0;
+        for (int i = 10; i <= 1; i--)
+        {
+            var result = mrc.Limit(input, max);
+            result.Should().BeApproximately(i * max, 0.01f);
+        }
+    }
+}
---
+++ Api/Helpers/MaxRateOfChange.cs
@@ -0,0 +1,28 @@
+namespace SheepHerding.Api.Helpers;
+
+public class MaxRateOfChange
+{
+    private double _nowValue;
+
+    public MaxRateOfChange(double init = 0.0)
+    {
+        _nowValue = init;
+    }
+    public double Limit(double input, double max)
+    {
+        if (Math.Abs(_nowValue - input) > max)
+        {
+            if (input > _nowValue)
+            {
+                _nowValue += max;
+            }
+            else
+            {
+                _nowValue -= max;
+            }
+            return _nowValue;
+        }
+
+        return input;
+    }
+}
