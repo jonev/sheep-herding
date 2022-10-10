2b601a9f81e5179e4347acac8619295b69f46e55
Jone Vassbø <jone.vassbo@gmail.com>
2022-10-10 21:54:45 +0200

    Bump version

--- push.sh
+++ push.sh
@@ -1,5 +1,5 @@
 echo "Building app"
-docker build -f ./Api/Dockerfile -t jonev/home-hosting:masters-v4 .
-docker push jonev/home-hosting:masters-v4
+docker build -f ./Api/Dockerfile -t jonev/home-hosting:masters-v5 .
+docker push jonev/home-hosting:masters-v5
 ssh -t jone@prod1 '/home/jone/repos/home-hosting/masters/deploy.sh'
 echo "Successfully deployed the app"
